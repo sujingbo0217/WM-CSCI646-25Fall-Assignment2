5e4bb4d53fd5b14c70368d2ac15b35d5f01995e9
Jaidyn Vankirk <vankirk.jaidyn@gmail.com>
2025-11-05 09:49:32 -0500

    Add files via upload

---
+++ DataLoader.py
@@ -0,0 +1,25 @@
+import torch
+from torch.utils.data import Dataset
+import pandas as pd
+from ast import literal_eval
+
+
+'''Create data loader'''
+
+class MovieDataset(Dataset):
+    def __init__(self, filename):
+        self.df = pd.read_csv(filename, converters={'input_x': literal_eval})
+        # print(self.df['input_x'])
+
+    
+    def __len__(self):
+        return len(self.df)
+    
+    def __getitem__(self, index):
+        input_x = self.df.loc[index, 'input_x']
+        label = self.df.loc[index, 'labels']
+        
+
+        return torch.tensor(input_x), torch.tensor(label,dtype=torch.float)
+        
+        
