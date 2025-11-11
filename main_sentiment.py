b6cfb9215e9e1d0c42fd613f0607ef03f49df941
Jaidyn Vankirk <vankirk.jaidyn@gmail.com>
2025-11-10 21:59:55 -0500

    Add files via upload

--- main_sentiment.py
+++ main_sentiment.py
@@ -17,12 +17,14 @@ from DataLoader import MovieDataset
 from LSTM import LSTMModel
 from glove_embedding import _get_embedding
 import time
+import wandb
+import sklearn
+import sklearn.metrics
 
 
 '''save checkpoint'''
-def _save_checkpoint(ckp_path, model, epoches, global_step, optimizer):
+def _save_checkpoint(ckp_path, model, epoches, optimizer):
     checkpoint = {'epoch': epoches,
-                'global_step': global_step,
                 'model_state_dict': model.state_dict(),
                 'optimizer_state_dict': optimizer.state_dict()}
 
@@ -30,6 +32,27 @@ def _save_checkpoint(ckp_path, model, epoches, global_step, optimizer):
 
 
 def main():
+    wandb.init(
+        project="Assignment 2",  # your project name
+        name="glove 300 embedding_dim Run",              # custom run name
+        config={
+            # Training parameters
+            "batch_size": 250,
+            "n_layers": 2,
+            "input_len": 150,
+            "embedding_dim": 300,
+            "hidden_dim": 25,
+            "output_size": 1,
+            "num_epochs": 15,
+            "learning_rate": 0.0005,
+            "clip": 5,
+            "load_cpt": False,
+            "ckp_path": 'cpt/name.pt',
+            "pretrain": True
+        }
+    )
+	
+    
     gpu_id = 0
     use_cuda = torch.cuda.is_available()
     if use_cuda:
@@ -39,35 +62,35 @@ def main():
     
     ## alternative method
     # torch.cuda.set_device(device=0) ## choose gpu number
-    print('device: 0', device)
+    print('device: 2', device)
 
     ## ---------------------------------------------------------
     ## please change the parameter settings by yourselves
     ## ---------------------------------------------------------
     mode = 'train'
-    Batch_size =300
-    n_layers = 1 ## choose 1-3 layers
+    Batch_size =wandb.config.batch_size
+    n_layers = wandb.config.n_layers ## choose 1-3 layers
 
     ## input seq length aligned with data pre-processing
-    input_len = 150
+    input_len = wandb.config.input_len
 
     ## word embedding length
-    embedding_dim = 50
+    embedding_dim = wandb.config.embedding_dim
 
     # lstm hidden dim
-    hidden_dim = 50
+    hidden_dim = wandb.config.hidden_dim
     # binary cross entropy
-    output_size = 1
-    num_epoches = 1
+    output_size = wandb.config.output_size
+    num_epoches = wandb.config.num_epochs
     ## please change the learning rate by youself
-    learning_rate = 0.002
+    learning_rate = wandb.config.learning_rate
     # gradient clipping
-    clip = 5
-    load_cpt = False #True
-    ckp_path = 'cpt/name.pt'
+    clip = wandb.config.clip
+    load_cpt = wandb.config.load_cpt
+    ckp_path = wandb.config.ckp_path
     # embedding_matrix = None
     ## use pre-train Glove embedding or not?
-    pretrain = False
+    pretrain = wandb.config.pretrain
 
     ##-----------------------------------------------------------------------
     ## Bonus (5%): complete code to add GloVe embedding file path below.
@@ -75,7 +98,7 @@ def main():
     ## "embedding_dim" defined above shoud be aligned with the dimension of GloVe embedddings
     ## if you do not want bonus, you can skip it.
     ##-----------------------------------------------------------------------
-    glove_file = 'path/glove.6B.200d.txt' ## change by yourself
+    glove_file = 'glove.6B/glove.6B.300d.txt' ## change by yourself
     
 
     ## ---------------------------------------------------------
@@ -85,10 +108,10 @@ def main():
     
     
     ## step 2: load training and test data from data loader [it is Done]
-    training_set = MovieDataset('training_data.csv')
+    training_set = MovieDataset('data/training_data.csv')
     training_generator = DataLoader(training_set, batch_size=Batch_size,\
                                     shuffle=True,num_workers=1)
-    test_set = MovieDataset('test_data.csv')
+    test_set = MovieDataset('data/test_data.csv')
     test_generator = DataLoader(test_set, batch_size=Batch_size,\
                                 shuffle=False,num_workers=1)
 
@@ -139,9 +162,12 @@ def main():
     print('*'*89)
     if mode == 'train':
         model.train()
+        global_step = 0
         for epoches in range(num_epoches):
+            total_loss = 0
+            total_accy = 0
             for x_batch, y_labels in training_generator:
-                
+                global_step += 1
                 x_batch, y_labels = x_batch.to(device), y_labels.to(device)
                 ##-----------------------------------------------
                 ## complete code to get predict result from model
@@ -152,6 +178,11 @@ def main():
                 ## complete code to get loss
                 ##-----------------------------------------------
                 loss = loss_fun(y_out, y_labels)
+                total_loss += loss
+                
+                y_pred = torch.round(y_out)
+                accy = sklearn.metrics.accuracy_score( y_pred.detach().cpu().numpy(), y_labels.detach().cpu().numpy())
+                total_accy += accy
 
                 ## step 8: back propagation [Done]
                 optimizer.zero_grad()
@@ -159,11 +190,16 @@ def main():
                 ## **clip_grad_norm helps prevent the exploding gradient problem in LSTMs.
                 nn.utils.clip_grad_norm_(model.parameters(), clip)
                 optimizer.step()
-                
+            
+            avg_train_loss = total_loss/len(training_generator)
+            avg_train_accy = total_accy/len(training_generator)
+            wandb.log({"Epoch": epoches, "Average Training Loss": avg_train_loss, "Average Training Accuracy": avg_train_accy})
             ##-----------------------------------------------   
             ## step 9: complete code below to save checkpoint
             ##-----------------------------------------------
             print("**** save checkpoint ****")
+            ckp_path = f'checkpoint/step_{epoch}.pt'
+            _save_checkpoint(ckp_path, model, epoches, optimizer)
             
     
     ##------------------------------------------------------------------
@@ -175,12 +211,24 @@ def main():
     
     model.eval()
     with torch.no_grad():
+        labels = []
+        preds = []
         for x_batch, y_labels in test_generator:
             x_batch, y_labels = x_batch.to(device), y_labels.to(device)
             y_out = model(x_batch)
             y_pred = torch.round(y_out)
             #I need to do the metrics
-
+            labels.extend(y_labels.cpu().numpy())
+            preds.extend(y_pred.cpu().numpy())
+   
+    labels = torch.tensor(labels)
+    preds = torch.tensor(preds)
+    test_accy = sklearn.metrics.accuracy_score(preds, labels)
+    test_recall = sklearn.metrics.recall_score(preds, labels, average='macro')
+    test_precision = sklearn.metrics.precision_score(preds, labels, average='macro')
+    test_f1 = sklearn.metrics.f1_score(preds, labels, average='macro')
+    
+    wandb.log({"Test Accuracy": test_accy, "Test Recall": test_recall, "Test Precision": test_precision, "Test F1": test_f1})
 
 if __name__ == '__main__':
     time_start = time.time()
